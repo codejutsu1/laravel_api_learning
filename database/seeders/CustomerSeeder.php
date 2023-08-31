13f2091c41896c594aad91d1c33937e0b60e488c
codejutsu1 <codejutsu@protonmail.com>
2023-08-31 01:50:08 +0100

    calling the customer seeder and correcting some errors in customer factory

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Customer;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CustomerSeeder extends Seeder
 {
